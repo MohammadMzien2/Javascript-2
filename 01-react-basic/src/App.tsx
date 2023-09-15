ef3d8ff247499b7a6201b6e107d7425af6f274c0
MohammadMzien2 <mohammadlu281@gmail.com>
2023-09-15 10:57:54 +0200

    fragment and make button for show and hide salary

--- 01-react-basic/src/App.tsx
+++ 01-react-basic/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
   ])
 
   const [salary, setSaraly] = useState(10)
+  const [showSaraly, setShowSalary] = useState(true)
 
   const handleButtonClick = () => {
     console.log("Clicks before first change:", clicks)
@@ -27,7 +28,7 @@ function App() {
 
     console.log("Clicks after first change:", clicks)
 
-    setClicks(prevState =>  prevState + 1 ) // prevState = 1, return 2
+    setClicks(prevState => prevState + 1) // prevState = 1, return 2
 
     console.log("Clicks after second change:", clicks)
 
@@ -61,35 +62,49 @@ function App() {
 
       <hr />
 
-      <p>Saraly per hour: {salary} &euro;</p>
+      {/*<button className='btn btn-primary' onClick={() => setShowSalary  (true)}>show Salary</button>
+      <button className='btn btn-primary'onClick={() => setShowSalary(false)}>Hide Salary</button>*/}
 
-      {salary < 10 &&
-        <div className='alert alert-warning'>You might want to change job</div>
+      <button className='btn btn-primary' onClick={() => setShowSalary(!showSaraly)}>Show/Hide Salary</button>
+
+
+      {showSaraly && (
+
+        <>
+
+          <p>Saraly per hour: {salary} &euro;</p>
+
+          {salary < 10 &&
+            <div className='alert alert-warning'>You might want to change job</div>
+          }
+
+          <div className='buttons'>
+            <div className='mb-1'>
+              <button className='btn btn-primary btn-lg' onClick={() => { handleChangeSalary(1) }}>
+                Raise 1 &euro;</button>
+
+              <button className='btn btn-warning btn-lg' onClick={() => { handleChangeSalary(-1) }}>
+                Decrease 1 &euro;</button>
+            </div>
+
+            <div className='mb-1'>
+              <button className='btn btn-success btn-lg'
+                onClick={() => { handleChangeSalary(5) }}>
+                Raise 5 &euro;
+              </button>
+              <button className='btn btn-danger btn-lg'
+                onClick={() => { handleChangeSalary(-5) }}>
+                Decrease 5 &euro;
+
+              </button>
+
+            </div>
+          </div>
+        </>
+      )
       }
 
-      <div className='buttons'>
-        <div className='mb-1'>
-          <button className='btn btn-primary btn-lg' onClick={() => { handleChangeSalary(1) }}>
-            Raise 1 &euro;</button>
-
-          <button className='btn btn-warning btn-lg' onClick={() => { handleChangeSalary(-1) }}>
-            Decrease 1 &euro;</button>
-        </div>
-
-        <div className='mb-1'>
-          <button className='btn btn-success btn-lg'
-            onClick={() => { handleChangeSalary(5) }}>
-            Raise 5 &euro;
-          </button>
-          <button className='btn btn-danger btn-lg'
-            onClick={() => { handleChangeSalary(-5) }}>
-            Decrease 5 &euro;
-
-          </button>
-
-        </div>
-
-      </div>
+      <hr />
 
       <h2>Posts</h2>
 
