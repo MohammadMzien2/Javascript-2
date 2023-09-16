c9bce618a99ad8bd465f62261262c30be5564a2f
MohammadMzien2 <mohammadlu281@gmail.com>
2023-09-16 17:07:39 +0200

    working clock

--- 03-React-clock/src/App.tsx
+++ 03-React-clock/src/App.tsx
@@ -1,10 +1,25 @@
 import './assets/scss/App.scss'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [time, setTime] = useState(() => {
+    console.log("I'm initing...")
+    return new Date().toLocaleTimeString()
+  })
+
+  useEffect(() => {
+    console.log('Starting clock...')
+    
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString())
+      console.log("tick")
+    }, 1000)
+  }, [])
+
   return (
     <div className='container'>
       <div className='display-1 text-center'>
-        00:13:37
+        {time}
       </div>
     </div>
   )
