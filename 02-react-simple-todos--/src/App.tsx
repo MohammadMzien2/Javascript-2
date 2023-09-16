cef3a9a59778bbebfce425de7a45cbed587c4a75
MohammadMzien2 <mohammadlu281@gmail.com>
2023-09-16 12:04:05 +0200

    deletea todo

--- 02-react-simple-todos--/src/App.tsx
+++ 02-react-simple-todos--/src/App.tsx
@@ -25,62 +25,69 @@ function App() {
     setTodos([...todos, newTodo])
 
     // Clear newTodoTitle state
-    setNewTodoTitle("") 
-  } 
+    setNewTodoTitle("")
 
-
-    const toggleTodo = (todo: Todo) => {
-      todo.completed = !todo.completed // man kan göra så istället för skriva if sats 
-      setTodos([...todos])
-
-      //if(todo.completed){
-      // todo.completed = false
-      //}else {
-      // todo.completed = true
-      //}
-    }
-
-    return (
-      <div className='container'>
-        <h1 className='mb-3'>React simple todos</h1>
-
-        <form onSubmit={handlesubmit} className='mb-3'>
-
-          <div className="input-group">
-
-            <input type="text"
-              className='form-control'
-              placeholder='Todo title'
-              onChange={e => setNewTodoTitle(e.target.value)}
-              value={newTodoTitle} />
-
-            <button type='submit' className='btn btn-success'>
-              Create
-            </button>
-          </div>
-        </form>
-
-        <ul className="todolist">
-
-          {todos.map((todo, index) => (
-
-            <li className={todo.completed ? "done" : ""} key={index}>
-
-              <span className="todo-title">{todo.title}</span>
-
-              <div className='ms-1'>
-
-                <span className="todo-delete" role='button'>🗑️</span>
-
-                <span className="todo-toggle" onClick={() => toggleTodo(todo)} role='button'>{todo.completed ? '✅' : '✅'}</span>
-              </div>
-            </li>
-
-          ))}
-        </ul>
-        <p className="status">{todos.filter(todo => todo.completed).length} of {todos.length} todos completed</p>
-      </div>
-    )
+  }
+  
+  const deleteTodo = (todoToDelete: Todo) => {
+    //set a new list of todos where the clicked todo is excluded
+    setTodos(todos.filter(todo => todo !== todoToDelete))
   }
 
-  export default App
+  const toggleTodo = (todo: Todo) => {
+    todo.completed = !todo.completed // man kan göra så istället för skriva if sats 
+    setTodos([...todos])
+
+    //if(todo.completed){
+    // todo.completed = false
+    //}else {
+    // todo.completed = true
+    //}
+  }
+
+  return (
+    <div className='container'>
+      <h1 className='mb-3'>React simple todos</h1>
+
+      <form onSubmit={handlesubmit} className='mb-3'>
+
+        <div className="input-group">
+
+          <input type="text"
+            className='form-control'
+            placeholder='Todo title'
+            onChange={e => setNewTodoTitle(e.target.value)}
+            value={newTodoTitle} />
+
+          <button type='submit' className='btn btn-success'>
+            Create
+          </button>
+        </div>
+      </form>
+
+      <ul className="todolist">
+
+        {todos.map((todo, index) => (
+
+          <li className={todo.completed ? "done" : ""} key={index}>
+
+            <span className="todo-title">{todo.title}</span>
+
+            <div className='ms-1'>
+
+              <span className="todo-delete" onClick={() => deleteTodo(todo)} role='button'>🗑️</span>
+
+              <span className="todo-toggle" onClick={() => toggleTodo(todo)} role='button'>{todo.completed ? '✅' : '✅'}</span>
+            </div>
+          </li>
+
+        ))}
+      </ul>
+
+        //show completed vs total number of todos
+      <p className="status">{todos.filter(todo => todo.completed).length} of {todos.length} todos completed</p>
+    </div>
+  )
+}
+
+export default App
