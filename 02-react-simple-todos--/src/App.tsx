79fa0e5c977a0fe23ff99c8fca44ad7a1a3066f0
MohammadMzien2 <mohammadlu281@gmail.com>
2023-09-16 10:59:27 +0200

    added todolist and rendering of list

--- 02-react-simple-todos--/src/App.tsx
+++ 02-react-simple-todos--/src/App.tsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
 import './assets/scss/App.scss'
+import { Todo, TodoList } from './types'
 
 function App() {
+  const [todos, setTodos] = useState<TodoList>([
+    { title: "Make Coffee", completed: true },
+    { title: "Drink Coffee", completed: false },
+    { title: "Drink More Coffee", completed: false },
+    { title: "Drink all the Coffee", completed: false },
+  ])
+
+
   return (
     <div className='container'>
       <h1>React simple todos</h1>
+
+      <ul className="todolist">
+
+        {todos.map((todo, index) => (
+
+          <li className={todo.completed ? "done" : "" } key={index}>
+
+            <span className="todo-title">{todo.title}</span>
+
+            <span className="todo-delete" role='button'>🗑️</span>
+
+            <span className="todo-toggle" role='button'>✅</span>
+
+          </li>
+
+        ))}
+      </ul>
+
     </div>
   )
 }
